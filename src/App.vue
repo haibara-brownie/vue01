77cc4a88a206334a3f687dda7380752797c5b439
haibarabrownie <haibarabrownie@gmail.com>
2024-03-10 17:09:55 +0800

    gitTest

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,25 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div id = "app">
+    <p>
+      数据驱动视图{{ age }}
+    </p>
+    <p>
+      视图驱动数据：
+      用户名：<input type="text" v-model="username"> {{ username }}
+    </p>
+  </div>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+<script>
+export default{
+  data(){
+    return{
+      //年龄
+      age:10,
+      username:"admin"
+    }
+  }
 }
+</script>
 
-nav {
-  padding: 30px;
-}
 
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
--- src/main.js
+++ src/main.js
@@ -2,4 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
+//关闭日志提醒
+// Vue.config.productionTip = false
+
 createApp(App).use(router).mount('#app')
