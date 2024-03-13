57e26096bfe76c063aa9aa7b9746207419b7fb44
haibarabrownie <haibarabrownie@gmail.com>
2024-03-13 18:05:55 +0800

    click事件及绑定

--- src/App.vue
+++ src/App.vue
@@ -8,8 +8,16 @@
       用户名：<input type="text" v-model="username"> {{ username }}
     </p>
     <button @click="onLogin">登录</button>
+    <hr>
+
+    <!--设置点击事件为取反，则每次点击会使状态取反-->
+    <button @click="isClick=!isClick" :class="{active:isClick}">
+      点击切换样式
+    </button>
   </div>
 </template>
+
+<!--template节点有且只能有一个，是根节点-->
 
 <script>
 export default{
@@ -17,15 +25,26 @@ export default{
     return{
       //年龄
       age:10,
-      username:"admin"
+      username:"admin",
+      isClick:false
     }
   },
   methods:{
+    //登陆事件，打印用户名
     onLogin(){
       console.log(this.username);
-    }
+    },
   }
 }
 </script>
+
+<!--样式控制，加scoped代表控制部分，不加代表控制全局-->
+
+<style scoped>
+.active{
+  background-color: blue;
+  color: white;
+}
+</style>
 
 
